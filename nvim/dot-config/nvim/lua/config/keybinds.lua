6403a769c45ad81e952393eabff850fff24f6c44
J. Ignacio Sardi <desardi131@gmail.com>
2026-02-10 14:34:05 +0100

    Configuracion inicial de nvim

---
+++ nvim/dot-config/nvim/lua/config/keybinds.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.keymap.set("i", "jj", "<Esc>")
+
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>x", ":x<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+vim.keymap.set("n", "<leader>Q", ":q!<CR>")
+
+vim.keymap.set("n", "<leader>so", function()
+    vim.cmd("so")
+end)
