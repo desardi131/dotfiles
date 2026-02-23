e300dc6aad03dce2a42aee67712edf32bc0325ad
J. Ignacio Sardi <desardi131@gmail.com>
2026-02-23 06:37:19 +0100

    Configurado el lsp para nvim

---
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable({
+                "lua_ls",
+                "html",
+                "intelephense",
+                "sqlls",
+                "bashls",
+                "jsonls",
+                "docker_compose_language_service",
+                "yamlls",
+                "markdown_oxide",
+                "vimls",
+                "emmet_language_server"
+            })
+        end,
+    }
+},
+
+vim.diagnostic.config({
+    virtual_text = true,
+    severity_sort = true,
+    float = {
+        style = 'minimal',
+        border = 'rounded',
+        source = 'if_many'
+    },
+})
