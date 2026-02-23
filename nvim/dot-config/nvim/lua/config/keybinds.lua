48301dba9a19ec4dd03324fb832cf420b0094859
J. Ignacio Sardi <desardi131@gmail.com>
2026-02-23 02:13:24 +0100

    Añadidos algunos atajos para copiar codigo y para mejorar el movimiento en los archivos

--- nvim/dot-config/nvim/lua/config/keybinds.lua
+++ nvim/dot-config/nvim/lua/config/keybinds.lua
@@ -11,3 +11,35 @@ vim.keymap.set("n", "<leader>Q", ":q!<CR>")
 vim.keymap.set("n", "<leader>so", function()
     vim.cmd("so")
 end)
+
+--[[ Highlight when yanking text
+Try it with `yap` in normal mode
+  See `:help vim.highlight.on_yank()`
+-- ]]
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- MOVIMIENTOS EN EL ARCHIVO
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Almacena el contenido pegado y no el sustituido al pegarlo
+-- Es necesario tener instalado un cliente del portapapeles (e.g. wl-copy)
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Para copiar
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Para borrar
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
